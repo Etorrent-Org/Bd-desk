4d62bd9c7a4eabfadeec4caf50b52f8e8dda57c5
Erwann Torrent <279009398+R1-Torrent@users.noreply.github.com>
2026-09-01 10:08:29 +0200

    fix: always enable custom scanner on mobile browsers

--- public/ean-scanner.js
+++ public/ean-scanner.js
@@ -1,6 +1,5 @@
 (()=>{
-  const isIOS=/iPad|iPhone|iPod/.test(navigator.userAgent)||(navigator.platform==='MacIntel'&&navigator.maxTouchPoints>1);
-  if(!isIOS&&'BarcodeDetector' in window)return;
+  window.__BD_EAN_SCANNER_VERSION='2026-09-01.3';
 
   const L=['0001101','0011001','0010011','0111101','0100011','0110001','0101111','0111011','0110111','0001011'];
   const G=['0100111','0110011','0011011','0100001','0011101','0111001','0000101','0010001','0001001','0010111'];
